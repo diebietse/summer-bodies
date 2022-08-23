9777d301ae997afe2edf81e05e30ab2556a99924
Jedri Visser <git@jedrivisser.com>
2022-08-23 22:02:45 +0200

    Remove weekly maintenance

--- functions/examples/print-fitcoin.ts
+++ functions/examples/print-fitcoin.ts
@@ -2,7 +2,7 @@
 // Requires:
 // * Valid config loaded on firestore (including strava credentials)
 // * A service-account.json file with a firestore service account in the project root directory
-// * Some activities this week in the configured Stava club
+// * Some activities this week in the configured Strava club
 
 import { Challenge } from "../src/challenge";
 import { Format } from "../src/format";
